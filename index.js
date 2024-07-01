97daf8dab640a9ec8eb727ba97e2461a1f38a830
BielGSouza <gabrielsouzaca05@gmail.com>
2024-07-01 13:59:48 -0300

    sobre mim e outros prontos

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-function Verificar(){
-    document.body.classList.toggle('dark-mode')
+function Verificar() {
+    var botao = document.getElementById('btndarkmode')
+    var mudar = document.getElementById('art-futuro-habilidades')
+    var divvazio = document.getElementById('vazio')
+    var txtfuturo = document.querySelectorAll('.txt-futuro-habilidades')
+    if (botao.checked) {
+        mudar.style.backgroundColor = "#171616"
+        divvazio.style.backgroundColor = "#171616"
+        mudar.style.transition = "1s"
+        divvazio.style.transition = "1s"
+
+        txtfuturo.forEach(elemento => {
+            elemento.style.color= '#F2F1EB'
+        })
+    } else {
+        mudar.style.backgroundColor = "#EBFDEF"
+        divvazio.style.backgroundColor ="#EBFDEF"
+
+        txtfuturo.forEach(elemento => {
+            elemento.style.color = '#1B1A1A'
+        })
+    }
 }
